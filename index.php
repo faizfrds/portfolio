99ed1d3a2b6ef8c9a12b2d54fa7d7eccdf65b9fc
Faiz Firdaus <faiz.firdaus.2004@gmail.com>
2023-01-12 12:08:39 +0700

    adding php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php include('front_end/header/header.html')?>
+
+
+        <!-- Text section -->
+        <div class="bg-img">
+            <div class="wrapper-content"> <!-- Relating to CSS wrapper -->
+                TEXT
+            </div>
+        </div>
+        
+        <!-- Footer -->
+        <div class="footer">
+            <div class="wrapper text-center"> <!-- Relating to CSS wrapper -->
+                <a style="text-decoration: none" href="#">FAIZ FIRDAUS</a> <!-- Relating to CSS text cebnter -->
+            </div>
+        </div>
+
+    </body>
+</html>
